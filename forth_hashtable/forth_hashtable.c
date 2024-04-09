ea4ead41184601f8c772e15646cf483c31a8d4ca
morgan burch <mburch1@luc.edu>
2024-04-09 02:25:37 +0000

    linked all external files to hashtable main, added new hashtable funcitons for user defined funcitons

--- forth_hashtable/forth_hashtable.c
+++ forth_hashtable/forth_hashtable.c
@@ -12,15 +12,17 @@ void destroy(GHashTable* hashtable) {
     g_hash_table_destroy(hashtable);
 }
 
+//FORTH primitive functions 
 void insert(GHashTable* hashtable, char* key, ForthFunction func) {
     g_hash_table_insert(hashtable, key, func);
 }
 
-
+//returns a callable function to use in user interface 
 ForthFunction lookup(GHashTable* hashtable, char* key) {
     return g_hash_table_lookup(hashtable, key);
 }
 
+//add funcitons to FORTH map 
 void add_all_functions(GHashTable* hashtable){
     insert(hashtable, "dup", (ForthFunction) int_stack_dup);
     insert(hashtable, "2dup", (ForthFunction) int_stack_2dup);
@@ -34,4 +36,13 @@ void add_all_functions(GHashTable* hashtable){
     insert(hashtable, "2drop", (ForthFunction) int_stack_2drop);
     insert(hashtable, "over", (ForthFunction) int_stack_over);
     insert(hashtable, "2over", (ForthFunction) int_stack_2over);
+}
+
+//user hashtable 
+void user_insert(GHashTable* hashtable, char* key, token_t* tokens) {
+    g_hash_table_insert(hashtable, key, tokens);
+}
+
+token_t* user_lookup(GHashTable* hashtable, char* key) {
+    return g_hash_table_lookup(hashtable, key);
 }
--- forth_hashtable/forth_hashtable.h
+++ forth_hashtable/forth_hashtable.h
@@ -1,6 +1,7 @@
-#include <glib.h> //lib for hash functions
+#include <glib.h> //hastable library
 #include <stdio.h>
 #include "int_stack.h"
+#include <token.h>
 
 
 typedef void (*ForthFunction)(int_stack_t *);
@@ -11,3 +12,5 @@ extern void destroy(GHashTable* hashtable);
 extern void add_all_functions(GHashTable* hashtable);
 extern void insert(GHashTable* hashtable, char* key, ForthFunction func);
 extern ForthFunction lookup(GHashTable* hashtable, char* key);
+extern void user_insert(GHashTable* hashtable, char* key, token_t* tokens);
+extern token_t* user_lookup(GHashTable* hashtable, char* key);
